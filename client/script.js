a2775c4edc56c74b9ee52a30ebbe0fd5f5e8b6d3
Abhi Khokhar <abhikhokhar2004@gmail.com>
2024-10-16 16:55:57 +0530

    Add files via upload

---
+++ client/script.js
@@ -0,0 +1,8 @@
+
+// JavaScript for toggling the mobile menu
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+});
+
+
