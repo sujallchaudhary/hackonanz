c6dba4125ec4cf82ee04c0057a2ac012f424cfa4
Ayush rawat <165577927+Ayu-Rawat@users.noreply.github.com>
2024-10-16 17:21:16 +0530

    make edits

--- client/script.js
+++ client/script.js
@@ -4,5 +4,3 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
     const mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.classList.toggle('hidden');
 });
-
-
